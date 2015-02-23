4e1cca033650d2d1b06dfa02d5b90d6575c18428
Crystal Contreras-Grossman & Mike Harris <student@epicodus.com>
2015-02-23 12:13:47 -0800

    add ability to search for restaurant

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -3,11 +3,13 @@ goodRestaurant.controller('RestaurantsCtrl', function StudentsCtrl($scope) {
   $scope.addRestaurant = function() {
     var newRestaurant = {
                           name: $scope.restaurant.restaurantName,
-                          description: $scope.restaurant.restaurantDescription
+                          description: $scope.restaurant.restaurantDescription,
+                          location: $scope.restaurant.restaurantLocation
                         }
   $scope.restaurants.push( newRestaurant );
   $scope.restaurant.restaurantName = null;
   $scope.restaurant.restaurantDescription = null;
+  $scope.restaurant.restaurantLocation = null;
   };
 
 });
